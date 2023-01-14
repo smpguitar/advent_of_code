c1b1fb568fba027328fcaa72de36382f8ec91e5d
Steve <smp_guitar@hotmail.com>
2023-01-13 21:47:36 -0600

    day eight test works on test data

--- lib/dayeight.dart
+++ lib/dayeight.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 decypher() {
 //get input
   String text = File(
-          '/Users/stephenparks/Development/advent_of_code/advent_of_code/bin/day_eight_test_data.txt')
+          '/Users/stephenparks/Development/advent_of_code/advent_of_code/bin/day_eight_data.txt')
       .readAsStringSync();
 
   // Split the text into lines in a list
@@ -25,12 +25,10 @@ decypher() {
   int currentColumn = 1;
 
   bool isVisible = false;
+  bool amIncrementing = false;
 
   int totalVisible = 0;
-  int additionaVisible = (grid.length * 2) + (lines.length * 2);
-
-  // print('from Right: $fromRight');
-  // print('grid one: ${grid[1][1]}');
+  int additionalVisible = (grid.length * 2) + (lines.length * 2) - 4;
 
   // create 4 functions isVisibleFromTop/Bottom/Left;Right
 
@@ -54,6 +52,7 @@ decypher() {
     }
     if (isVisible == true) {
       print('incrementing total visible');
+      amIncrementing = true;
       totalVisible = totalVisible + 1;
       isVisible == false;
     }
@@ -79,6 +78,7 @@ decypher() {
     }
     if (isVisible == true) {
       print('incrementing total visible');
+      amIncrementing = true;
       totalVisible = totalVisible + 1;
       isVisible == false;
     }
@@ -105,6 +105,34 @@ decypher() {
     if (isVisible == true) {
       print(
           '${grid[currentRow][currentColumn]} is visible from right.  incrementing total visible');
+      amIncrementing = true;
+      totalVisible = totalVisible + 1;
+      isVisible == false;
+    }
+  }
+
+  isVisibleFromLeft() {
+    for (int i = 0; i <= currentColumn - 1; i++) {
+      print('current Column minus 1: ${currentColumn - 1}');
+      print(
+          'current evaluated item:  row: $currentRow column: $i specific value: ${grid[currentRow][i]}');
+      print(
+          'comparison item: current row: $currentRow  column: $currentColumn specific value: ${grid[currentRow][currentColumn]}');
+      if (grid[currentRow][currentColumn] <= grid[currentRow][i]) {
+        print('will not be visible');
+        isVisible = false;
+        break;
+      }
+      if (grid[currentRow][currentColumn] > grid[currentRow][i]) {
+        isVisible = true;
+
+        print('is visible at row $currentRow, column $currentColumn');
+      }
+    }
+    if (isVisible == true) {
+      print(
+          '${grid[currentRow][currentColumn]} is visible from right.  incrementing total visible');
+      amIncrementing = true;
       totalVisible = totalVisible + 1;
       isVisible == false;
     }
@@ -115,16 +143,29 @@ decypher() {
     // print('row $row: ${lines[row]}');
 
     for (int column = 1; column < grid[0].length - 1; column++) {
+      amIncrementing = false;
       currentColumn = column;
       print('column $column');
       print('current row minus 1:  ${currentRow - 1}');
       print('grid lenth: ${grid[0].length}');
-      // isVisibleFromTop();
-      // isVisibleFromBottom();
-      isVisibleFromRight();
+      if (amIncrementing == false) {
+        isVisibleFromTop();
+      }
+      if (amIncrementing == false) {
+        isVisibleFromBottom();
+      }
+      if (amIncrementing == false) {
+        isVisibleFromRight();
+      }
+      if (amIncrementing == false) {
+        isVisibleFromLeft();
+      }
     }
   }
 //function to choose place in list to compare
-  print('total visible = $totalVisible');
+  print('${grid[0].length}');
+  print('inner visible = $totalVisible');
+  print('outer visible = $additionalVisible');
+  print('total visible = ${additionalVisible + totalVisible}');
 //compare values if > add to visible
 }
