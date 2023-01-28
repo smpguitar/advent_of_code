46cfcf06ffd86ae75e32ec6b001f78cc6ec59a6e
Steve <smp_guitar@hotmail.com>
2023-01-28 15:28:08 -0600

    dayten removed commas from answer

--- lib/dayten_bonus.dart
+++ lib/dayten_bonus.dart
@@ -14,27 +14,29 @@ decypher() {
       lines.map((row) => row.split(" ").toList()).toList();
   // List<int> signalStrengths = [];
   List<List<String>> display = List.generate(6, (_) => []);
-  List<String> currentRow = [];
-  // print(display);
+
+//set variables
   int cycles = 0;
   int currentValue = 1;
   int logLevel = 40;
   int listNumber = 0;
   List<int> sprite = [0, 1, 2];
 
+//create a break to move to next line of display
   void checkCycles() {
     if (cycles / logLevel == 1) {
-      // logSignalStrength();
       cycles = 0;
       listNumber = listNumber + 1;
     }
   }
 
+//set the Sprite
   void setSprite() {
     sprite.clear();
     sprite.addAll([currentValue - 1, currentValue, currentValue + 1]);
   }
 
+//determine what gets diplayed on screen . == blank # == lit pixel
   void addToList() {
     if (display[listNumber].length == sprite[0] ||
         display[listNumber].length == sprite[1] ||
@@ -45,6 +47,7 @@ decypher() {
     }
   }
 
+//process instructions
   for (int i = 0; i <= moves.length - 1; i++) {
     if (moves[i][0] == 'noop') {
       checkCycles();
@@ -64,9 +67,8 @@ decypher() {
       checkCycles();
     }
   }
-  // int result = signalStrengths.fold(0, (k, l) => k + l);
-  // print(result);
+  //print result
   for (int z = 0; z <= display.length - 1; z++) {
-    print(display[z]);
+    print(display[z].join());
   }
 }
